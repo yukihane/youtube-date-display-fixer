40a040ca6fbc9a3a75b0e074626f4345412ea3ff
yukihane <yukihane.feather@gmail.com>
2022-06-28 05:27:14 +0900

    一旦完成

---
+++ js/replace.js
@@ -0,0 +1,20 @@
+console.log("replace.js executed");
+
+const replace = () => {
+  const date = document.querySelector(
+    "#info-strings.ytd-video-primary-info-renderer"
+  );
+  const count = document.querySelector("span.ytd-video-view-count-renderer");
+
+  if (date && count) {
+    const dateText = date.innerText.toString();
+    console.log(dateText);
+    count.innerText = `${dateText} ` + count.innerText;
+    date.innerText = "";
+    console.log(count.innerText);
+  } else {
+    console.log("empty text");
+  }
+};
+
+setTimeout(replace, 500);
