9878f2c2aa1d632c295c338c194358ee8f0aee18
yukihane <yukihane.feather@gmail.com>
2022-07-01 14:00:28 +0900

    Uncaught SyntaxError: Identifier 'replace' has already been declared のエラーが出るので、即時実行匿名関数として実装

--- js/replace.js
+++ js/replace.js
@@ -1,18 +1,20 @@
-const replace = () => {
-  const date = document.querySelector(
-    "#info-strings.ytd-video-primary-info-renderer"
-  );
-  const count = document.querySelector("span.ytd-video-view-count-renderer");
+(() => {
+  const replace = () => {
+    const date = document.querySelector(
+      "#info-strings.ytd-video-primary-info-renderer"
+    );
+    const count = document.querySelector("span.ytd-video-view-count-renderer");
 
-  if (date && count) {
-    const dateText = date.innerText.toString();
-    console.log(dateText);
-    count.innerText = `${dateText} ` + count.innerText;
-    date.innerText = "";
-    console.log(count.innerText);
-  } else {
-    console.log("empty text");
-  }
-};
+    if (date && count) {
+      const dateText = date.innerText.toString();
+      console.log(dateText);
+      count.innerText = `${dateText} ` + count.innerText;
+      date.innerText = "";
+      console.log(count.innerText);
+    } else {
+      console.log("empty text");
+    }
+  };
 
-setTimeout(replace, 500);
+  setTimeout(replace, 500);
+})();
